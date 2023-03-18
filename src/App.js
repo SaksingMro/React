57b86030b91a397f6625f48494cce1b3001dd12f
Saksing <saksing927@gmail.com>
2023-03-18 10:27:46 +0600

    Uploaded

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import LoadCountries from './component/LoadCountries';
+const App=()=>{
+  return(
+    <div>
+      <LoadCountries/>
+    </div>
+  )
+};
+
+export default App;
